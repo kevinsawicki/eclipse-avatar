e19cfb00e000be6e7a86db71564cbb60d86c6448
Kevin Sawicki <kevin@github.com>
2011-03-18 20:57:08 -0700

    Add class to run an IAvatarCallback on the UI-thread

---
+++ org.github.avatar.ui/src/org/github/avatar/ui/AvatarDisplayCallback.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package org.github.avatar.ui;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * 
+ * Class to wrap an {@link IAvatarCallback} instance and run it as a runnable on
+ * the UI-thread.
+ * 
+ * @author Kevin Sawicki (kevin@github.com)
+ */
+public class AvatarDisplayCallback extends AvatarCallbackAdapter {
+
+	private IAvatarCallback wrapped;
+	private boolean async = true;
+
+	/**
+	 * Create avatar display callback
+	 * 
+	 * @param callback
+	 */
+	public AvatarDisplayCallback(IAvatarCallback callback) {
+		Assert.isNotNull(callback, "Callback cannot be null");
+		this.wrapped = callback;
+	}
+
+	/**
+	 * Set async execution of wrapped callback
+	 * 
+	 * @param async
+	 * @return this callback
+	 */
+	public AvatarDisplayCallback setAsync(boolean async) {
+		this.async = async;
+		return this;
+	}
+
+	private void displayExec(Runnable runnable) {
+		Display display = PlatformUI.getWorkbench().getDisplay();
+		if (this.async) {
+			display.asyncExec(runnable);
+		} else {
+			display.syncExec(runnable);
+		}
+	}
+
+	/**
+	 * @see org.github.avatar.ui.AvatarCallbackAdapter#loaded(org.github.avatar.ui.Avatar)
+	 */
+	public void loaded(final Avatar avatar) {
+		Runnable runnable = new Runnable() {
+
+			public void run() {
+				wrapped.loaded(avatar);
+			}
+		};
+		displayExec(runnable);
+	}
+
+	/**
+	 * @see org.github.avatar.ui.AvatarCallbackAdapter#error(java.lang.Exception)
+	 */
+	public void error(final Exception exception) {
+		Runnable runnable = new Runnable() {
+
+			public void run() {
+				wrapped.error(exception);
+			}
+		};
+		displayExec(runnable);
+	}
+
+}
